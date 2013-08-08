77bd5cd9d1b9e9dc96aaebd18b457432fbea74f7
Simone Pescina <s.pescina@gmail.com>
2013-08-08 11:13:21 +0200

    startup

---
+++ src/Spescina/Seorules/SeorulesServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Spescina\Seorules;
+
+use Illuminate\Support\ServiceProvider;
+
+class SeorulesServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
