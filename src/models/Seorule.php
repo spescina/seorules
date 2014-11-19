f536fb3d4cab4f5faf6569ffd984d73c2c23c595
Simone Pescina <s.pescina@gmail.com>
2014-11-19 18:15:03 +0100

    disabled laravel model timestamps

--- src/models/Seorule.php
+++ src/models/Seorule.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Config;
 
 class Seorule extends \Eloquent {
+    
+    public $timestamps = false;
 
     public function __construct(array $attributes = array())
     {
